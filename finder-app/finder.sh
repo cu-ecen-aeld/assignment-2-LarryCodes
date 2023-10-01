bed0c4b52f599f1cb02c8f76a41a6aa3087b28ac
LarryCodes <lawrenceokongo16@gmail.com>
2023-10-01 18:32:36 +0300

    Completed assignment 1 files.

---
+++ finder-app/finder.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Implementation file for assignment 1
+
+if [ $# -lt 2 ]
+then
+    echo "Requires 2 arguments, the files directorry and searchstring: Found $#"
+    echo "Usage: $0 <filesdir> <searchstr>"
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d "$filesdir" ]
+then
+    echo "Argument 1 should be a valid directory: ${filesdir}"
+    echo "Usage: $0 <filesdir> <searchstr>"
+    exit 1
+fi
+
+num_files=$(find "$filesdir" -type f | wc -l)
+num_matches=$(grep -R "$searchstr" "$filesdir" | wc -l)
+
+echo "The number of files are $num_files and the number of matching lines are $num_matches"
---
+++ finder-app/writer.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Implementation of assignment 1 writer
+
+if [ $# -lt 2 ]
+then
+    echo "Requires 2 arguments, the file path and writestring: Found $#"
+    echo "Usage: $0 <filepath> <writestring>"
+    exit 1
+fi
+
+writefile=$1
+writestr=$2
+
+# First check if directory exists
+file_path=$(dirname "$writefile")
+if [ ! -d $file_path ]
+then
+    mkdir -p "$file_path"
+    # Exit if making directory is not successful
+    if [ $? -ne 0 ]
+    then
+        echo "Failed creating directory structure!"
+        exit 1
+    fi
+fi
+
+echo "$writestr" > "$writefile"
+
+if [ $? -ne 0 ]
+then
+    echo "Failed to create and write file."
+    exit 1
+fi
+
+echo "File successfully created."
